39e2771fb37dc7f9001420af3165a6effd58d51b
Yesra Souliman <your_email@example.com>
2025-12-24 02:18:57 +0300

    اشعارات

--- database/migrations/2025_12_23_231625_create_notifications_table.php
+++ database/migrations/2025_12_23_231625_create_notifications_table.php
@@ -4,22 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('notifications', function (Blueprint $table) {
-            $table->id();
+            $table->id('notification_id');
+
+            $table->string('title', 20);           // عنوان الإشعار
+            $table->string('content', 255);        // محتوى الإشعار
+
+            // الاداري الذي أنشأ الإشعار
+            $table->unsignedBigInteger('created_by');
+
             $table->timestamps();
+
+            // ربط مع جدول users
+            $table->foreign('created_by')
+                  ->references('id')
+                  ->on('users')
+                  ->onDelete('cascade'); // إذا حذف الاداري يحذف إشعاراته
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('notifications');
